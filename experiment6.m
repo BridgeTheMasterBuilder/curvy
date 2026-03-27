71eb8ecc6d401f81f0c5f579963afc6070847499
svartursandur <alexanderbjartur@mail.com>
2026-03-27 14:59:24 +0000

    bætti við, fplot er chattað, ekki setja inn

--- experiment6.m
+++ experiment6.m
@@ -7,4 +7,39 @@
 function leng = experiment6()
   fx = @(t) 1 + 6*t + 6*t^2 - 9*t^3;
   fy = @(t) 1 + 6*t -9*t^2 + 4*t^3;
+  p = @(t) [fx(t), fy(t)];
+
+  % leysa
+  john = newton_partition(p, 6);
+  leng = arc_length(p, 0, 1, 0.001);
+  point = p(0);
+
+  figure
+  cla
+  fplot(fx, fy, [0 1], 'blue', 'LineWidth', 3)
+  axis square
+  set(gca,'XLim',[0 8],'YLim',[0 8],'SortMethod','childorder','visible', 'on');
+
+  partition_points = zeros(length(john), 2);
+  for k = 1:length(john)
+    partition_points(k, :) = p(john(k));
+  end
+  hold on
+  plot(partition_points(:,1), partition_points(:,2), 'ko', 'MarkerFaceColor', 'y', 'MarkerSize', 8)
+
+  ball=line('color','r','Marker','o','MarkerSize',10,'LineWidth',2,'xdata',[],'ydata',[]);
+  x = point(1);
+  y = point(2);
+
+  set(ball,'xdata',x,'ydata',y); drawnow; pause(0.01)
+
+  for k = linspace(0,1,100)
+    point = p(k);
+    x = point(1);
+    y = point(2);
+    set(ball,'xdata',x,'ydata',y);
+    drawnow;
+    pause(0.01)
+  end
+
 end
--- newton_partition.m
+++ newton_partition.m
@@ -1,11 +1,10 @@
-function path_length(f, n)
-  TOL = 0.001;
+function t = newton_partition(f, n)
+  TOL = 0.00001;
   lengd = arc_length(f,0,1,TOL);
   t = zeros(1, n+1); t(1) = 0; t(n+1) = 1;
-  n = 6;
-  for k = 1:6
+  for k = 1:n-1
     target_length = (k/n) * lengd;
     g = @(x) arc_length(f,0,x,TOL) - target_length;
-    t(k+1) = newton(g, t(k), 1 TOL);
+    t(k+1) = newton(g, t(k), 1, TOL);
   end
 end
