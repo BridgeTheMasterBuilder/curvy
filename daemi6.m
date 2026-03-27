9f5b02bae11b54fbcf3868ea8eb9cd5c993cbb7e
Master Builder <gylfi@stebbi.is>
2026-03-27 23:47:43 +0000

    3a.png lagað

--- daemi6.m
+++ daemi6.m
@@ -13,3 +13,41 @@ fplot(x, y, [0 1]);
 axis([-3.5 2.5 -3 3]);
 
 hold on;
+
+ball = line('Color','r','Marker','o','MarkerSize',10,'LineWidth',2,'XData',[],'YData',[]);
+
+while t <= 1.01
+    xt = x(t);
+    yt = y(t);
+
+    set(ball, 'XData', xt, 'YData', yt); 
+    drawnow; 
+    pause(0.01);
+
+    t = t + 0.01;
+end
+
+figure;
+
+fplot(x, y, [0 1]);
+
+axis([-3.5 2.5 -3 3]);
+
+hold on;
+
+ball = line('Color','r','Marker','o','MarkerSize',10,'LineWidth',2,'XData',[],'YData',[]);
+
+s = 0.01;
+
+while s <= 1
+    t_s = t_star(dx,dy,s);
+    
+    xt = x(t_s);
+    yt = y(t_s);
+
+    set(ball, 'XData', xt, 'YData', yt); 
+    drawnow; 
+    pause(0.01);
+
+    s = s + 0.01;
+end
